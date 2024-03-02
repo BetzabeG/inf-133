f4f38d07c2749899125800e86a3aefde6b5de5ea
BetzabeG <betzabegutierrezmorales@gmail.com>
2024-03-01 21:30:55 -0400

    Practica sobre los tipos de servicios de web SOAP Y REST

--- soap/soap_client.py
+++ soap/soap_client.py
@@ -3,12 +3,12 @@ client = Client('http://localhost:8000')
 
 result = client.service.Saludar(nombre="Betzabe")
 print(result)
-
-suma = client.service.SumaDosNumeros(num1=10,num2=14)
-print(suma)
+num1=10
+num2=14
+suma = client.service.SumaDosNumeros(num1=num1,num2=num2)
+print(f"La suma de {num1} y {num2} es: {suma}",)
 
 cadena = "radar"
 resultado_palindromo = client.service.CadenaPalindromo(cadena=cadena)
-print(cadena, resultado_palindromo)
+print(f"{cadena} es palindromo ? {resultado_palindromo}")
 
-#Entrega por GIT HUB enlace repositorio a GitHub
